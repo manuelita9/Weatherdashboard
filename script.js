49abcadf957446cd69147f345796d829b8f29121
manuela medina <manu573@live.com>
2023-04-17 10:56:16 -0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var searchBtn = document.getElementById("searchBtn");
-var historyBtn = document.getElementById("historyBtn");
 var historyContainer = document.getElementById("history");
 var searchInput = document.getElementById("searchInput");
 var apiKey = "ae8dc33d91f6926ff7f6df500e291f80";
