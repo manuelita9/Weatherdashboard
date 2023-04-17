34b688de626de0d31ed9f87045712258becd27ba
manuela medina <manu573@live.com>
2023-04-17 04:10:44 -0400

    updates

---
+++ script.js
@@ -0,0 +1,108 @@
+var searchBtn = document.getElementById("searchBtn");
+var historyBtn = document.getElementById("historyBtn");
+var historyContainer = document.getElementById("history");
+var searchInput = document.getElementById("searchInput");
+var apiKey = "ae8dc33d91f6926ff7f6df500e291f80";
+var cityName = document.getElementById("cityName");
+var temp = document.getElementById("temp");
+var humidity = document.getElementById("humidity");
+var wind = document.getElementById("wind");
+
+// server side api 
+function getCity() {
+  var city = searchInput.value;
+  var currentUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    apiKey;
+  var forecastUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=" +
+    apiKey;
+
+  fetch(currentUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      displayCurrentWeather(data);
+      saveToLS(data);
+      $(".historyBtn").remove();
+      createHistoryBtn();
+    });
+
+  fetch(forecastUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      $(".removeRow").remove();
+      displayForecastWeather(data);
+    });
+}
+
+function displayCurrentWeather(data) {
+  let kelvin = data.main.temp;
+  let fahrenheit = 1.8 * (kelvin - 273) + 32;
+  let iconCode = data.weather[0].icon;
+  let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+  cityName.textContent = data.name + " " + dayjs().format("ddd MM/DD/YYYY");
+  temp.textContent = "Temperature: " + fahrenheit.toFixed(2) + " °F";
+  humidity.textContent = "Humidity: " + data.main.humidity + "%";
+  wind.textContent = "Wind: " + data.wind.speed + " MPH";
+  $("#weatherIcon").attr("src", iconUrl);
+}
+
+function displayForecastWeather(data) {
+  for (i = 6; i <= 38; i += 8) {
+    let kelvin = data.list[i].main.temp;
+    let fahrenheit = 1.8 * (kelvin - 273) + 32;
+    let iconCode = data.list[i].weather[0].icon;
+    let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+
+    let dateEl = document.createElement("li");
+    let iconEl = document.createElement("img");
+    let tempEl = document.createElement("li");
+    let windEl = document.createElement("li");
+    let humidityEl = document.createElement("li");
+
+    dateEl.setAttribute("class", "removeRow");
+    iconEl.setAttribute("class", "removeRow");
+    tempEl.setAttribute("class", "removeRow");
+    windEl.setAttribute("class", "removeRow");
+    humidityEl.setAttribute("class", "removeRow");
+
+    let dateString = data.list[i].dt_txt;
+    let date = moment(dateString);
+
+    dateEl.textContent = date.format("ddd MM/DD/YYYY");
+    iconEl.setAttribute("src", iconUrl);
+    tempEl.textContent = "Temperature: " + fahrenheit.toFixed(2) + " °F";
+    windEl.textContent = "Wind: " + data.list[i].wind.speed + " MPH";
+    humidityEl.textContent = "Humidity: " + data.list[i].main.humidity + "%";
+
+    $("#forecast" + i).append(dateEl);
+    $("#forecast" + i).append(iconEl);
+    $("#forecast" + i).append(tempEl);
+    $("#forecast" + i).append(windEl);
+    $("#forecast" + i).append(humidityEl);
+  }
+}
+
+function saveToLS(data) {
+  localStorage.setItem(data.name, data.name);
+}
+
+
+historyContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("historyBtn")) {
+    searchInput.value = e.target.innerHTML;
+    getCity();
+  }
+});
+
+searchBtn.addEventListener("click", getCity);
