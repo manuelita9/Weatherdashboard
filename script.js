e40b3499c71de0fb8db3e3fe26e74056b416b9c2
manuela medina <manu573@live.com>
2023-04-17 23:50:33 -0400

    more vars

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ var historyContainer = document.getElementById("history");
 var searchInput = document.getElementById("searchInput");
 var apiKey = "ae8dc33d91f6926ff7f6df500e291f80";
 var cityName = document.getElementById("cityName");
+var temp = document.getElementById("temp");
+var humidity = document.getElementById("humidity");
