ea1fb391df465d881fda0ed86a3f4be548d92df8
manuela medina <manu573@live.com>
2023-04-17 23:47:05 -0400

    update vars cod

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 var searchBtn = document.getElementById("searchBtn");
 var historyBtn = document.getElementById("historyBtn");
+var historyContainer = document.getElementById("history");
+var searchInput = document.getElementById("searchInput");
