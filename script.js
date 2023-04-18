d1905bde5f1b486c00a89ddb88e8e1d2751630cc
manuela medina <manu573@live.com>
2023-04-17 23:50:04 -0400

    more vars

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ var searchBtn = document.getElementById("searchBtn");
 var historyBtn = document.getElementById("historyBtn");
 var historyContainer = document.getElementById("history");
 var searchInput = document.getElementById("searchInput");
+var apiKey = "ae8dc33d91f6926ff7f6df500e291f80";
+var cityName = document.getElementById("cityName");
